d3f7d549a89883781da90e112e2ccc41bedfb141
Abel Dimas <avtw37@gmail.com>
2019-11-20 09:30:29 -0800

    added temperature and summary

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector("location-timezone");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const api = `https://api.darksky.net/forecast/3227e3ee173f36abf41f0b7835f415fc/${lat},${long}`;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/3227e3ee173f36abf41f0b7835f415fc/${lat},${long}`;
 
       fetch(api)
         .then(response => {
@@ -15,6 +21,11 @@ window.addEventListener("load", () => {
         })
         .then(data => {
           console.log(data);
+          const { temperature, summary } = data.currently;
+          // Set DOM Elements from the API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+
         });
     });
   }
